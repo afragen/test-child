3401e2d45de62b33922944a92267fcd96664fa0c
Andy Fragen <andy@thefragens.com>
2014-03-29 15:56:10 -0700

    comment my tribe responsive css

--- functions.php
+++ functions.php
@@ -96,8 +96,8 @@ function add_legend_explanation($html) {
 		. $html;
 }
 
-add_filter('tribe_events_mobile_breakpoint','tribe_events_mobile_breakpoint_override');
-function tribe_events_mobile_breakpoint_override ($original) {
+add_filter( 'tribe_events_mobile_breakpoint', 'tribe_events_mobile_breakpoint_override' );
+function tribe_events_mobile_breakpoint_override( $original ) {
 	// Return the minimum width (in pixels) that a screen can be before before being switched to responsive view
 	return 650;
 }
